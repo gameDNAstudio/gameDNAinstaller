3e0f34b98fb0d035c0f2da19a0bb258f5ae25b9a
gameDNA <office@gamednastudio.com>
2019-02-20 00:14:27 +0000

    Update copyrights

--- Source/gameDNAinstaller/Private/GameDNAinstallerStyle.cpp
+++ Source/gameDNAinstaller/Private/GameDNAinstallerStyle.cpp
@@ -1,6 +1,6 @@
 // gameDNA installer
 // Created by Patryk Stepniewski
-// Copyright (c) 2014-2018 gameDNA Ltd. All Rights Reserved.
+// Copyright (c) 2014-2019 gameDNA Ltd. All Rights Reserved.
 
 #include "GameDNAinstallerStyle.h"
 #include "Runtime/Engine/Public/Slate/SlateGameResources.h"
